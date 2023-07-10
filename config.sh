e02404eb20aefe0be9f62cf5ff0b4d1e31f08082
Syed Rizvi <113552355+playerunknown1512@users.noreply.github.com>
2023-07-10 22:27:44 +0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,15 +2,15 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
+export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
+export DEVICE="ums9230_nico" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_realme_ums9230_nico/" # device tree link
+export DT_BRANCH="ums9230_nico_Natv-user-12-SP1A.210812.016-1605-release-keys" # device tree branch
+export VENDOR="realme" # device manufacturer or vendor
+export TARGET="vendorboot" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
